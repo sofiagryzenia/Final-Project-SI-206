d199e5c968c206e2ef0ca7c6afd88a656c4a9b9f
devitale <112510008+devitale@users.noreply.github.com>
2022-12-11 22:49:37 -0500

    Add files via upload

--- Spotify.py
+++ Spotify.py
@@ -6,31 +6,76 @@ import csv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
+from spotipy.oauth2 import SpotifyOAuth
+import matplotlib.pyplot as plt
 
 def spotify():
-    sp = spotipy.Spotify()
-    cid = "5b023ed3b67b4648a542d812ee5974f4"
-    secret = "5b3c316393c74cef8ab027e096757142"
+    cid = "3641e3928ecd4cf48421e3cf84cd31dc"
+    secret = "b16fb2fa6c7143daa22a8fc1c8c918f2"
+    
     os.environ['SPOTIPY_CLIENT_ID']= cid
     os.environ['SPOTIPY_CLIENT_SECRET']= secret
     os.environ['SPOTIPY_REDIRECT_URI']='http://localhost:8888/callback'
-    username =""
+
+    scope = "user-top-read"
     client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    scope = 'user-top-read'
-    token = util.prompt_for_user_token(cid, scope) 
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_top_tracks(limit=50,offset=0,time_range='medium_term')
-    if token:
-        for song in range(50):
-            list = []
-            list.append(results)
-            with open('top50_data.json', 'w', encoding='utf-8') as f:
-                json.dump(list, f, ensure_ascii=False, indent=4)
-        else:
-            print("Enter valid Username to get token for", username)
+    sp = spotipy.Spotify(auth_manager= SpotifyOAuth(scope = scope, redirect_uri= 'http://localhost:8888/callback'
+, client_id= cid, client_secret= secret ))
+
+    
+
+    playlists = sp.user_playlists('spotify')
+
+    results = sp.current_user_top_tracks(limit = 10, offset = 0, time_range = 'medium_term' )
+    items = results["items"]
+    #print(len(items))
+    popularity = []
+    names = []
+    for item in items:
+        popularity.append(item["popularity"])
+        names.append(item["name"])
+        #for key in item:
+            #print (key, item[key])
+
+       # print (item, results[item])
+        #print("==================================================================")
+
+
+    print (popularity)
+    print (names)
+
+    #fig, ax = plt.subplots()
+
+    #fruits = ['apple', 'blueberry', 'cherry', 'orange']
+    #counts = [40, 100, 30, 55]
+   #bar_labels = ['red', 'blue', '_red', 'orange']
+    #ar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+    #ax.bar(names, popularity)
+    #ax.bar(names, popularity, label=bar_labels, color=bar_colors)
+
+    #ax.set_ylabel('popularity')
+    #ax.set_title('song name')
+    #ax.legend(title='Fruit color')
+
+    #plt.show() 
+
+    y_axis = names
+    x_axis = popularity
+
+    plt.barh(y_axis, x_axis)
+    plt.title('title name')
+    plt.ylabel('y axis name')
+    plt.xlabel('x axis name')
+    plt.show()
+
+    
+
+    
+    
+    
+    
+
 
 
 
@@ -62,3 +107,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    #https://spotipy.readthedocs.io/en/2.9.0/
+    #https://rdrr.io/cran/spotifyr/man/get_my_top_artists_or_tracks.html
+    #https://developer.spotify.com/documentation/general/guides/authorization/scopes/
