a29817ee4393d5591dcbb3ffcd44576d5ada2de9
devitale <112510008+devitale@users.noreply.github.com>
2022-12-12 22:09:38 -0500

    Add files via upload

--- Visualizations.py
+++ Visualizations.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import geopandas as gpd
 
 
 def setUpDatabase(db_name):
@@ -14,10 +15,67 @@ def setUpDatabase(db_name):
     return cur, conn
 
 
+def events_visualization(cur,csvfile):
+
+    
+        ArtistNextEvent = pd.read_csv("ArtistNextEvent.csv")
+
+        ArtistNextEvent.head()
+               
+        
+        world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+        world.plot(figsize=(18,10))
+        plt.scatter(ArtistNextEvent.Longitude, ArtistNextEvent.Latitude, s=15, color="red", alpha=0.7)
+        plt.xlabel("Longitude")
+        plt.ylabel("Latitude")
+        plt.title("Concert Locations from the Current Top 25 Artists")
+        plt.show()
+
+ 
+def streams_visualzation(cur,csvfile):
+    
+    data = pd.read_csv("TotalTopStreams.csv")
+    
+    df = pd.DataFrame(data)
+
+    X = list(df.iloc[:, 0])
+
+    Y = list(df.iloc[:, 1])
+
+    plt.bar(X, Y, color='midnightblue')
+    plt.title("Total Streams for Top 25 Artists")
+    plt.xlabel("Artist Name")
+    plt.ylabel("Number of Streams in Billions")
+   
+    plt.xticks(rotation=50,fontsize=4,weight='bold')
+    
+    
+    plt.show()
+
+def num_concerts_visualzation(cur,csvfile):
+    
+    data = pd.read_csv("ArtistNextEvent.csv")
+
+    data.head()
+    
+    df = pd.DataFrame(data)
+    X = list(df.iloc[:, 1])
+    Y = list(df.iloc[:, 2])
 
 
 
-def billions(curr, csvfile):
+    plt.bar(X, Y, color='purple')
+    plt.title("Num of Concerts For Top 18 Artists From 2022-12-13,2023-12-9",weight='bold')
+    plt.xlabel("Artist Name")
+    plt.ylabel("Num of Concerts in the next year")
+   
+    plt.xticks(rotation=50,fontsize=7,weight='bold')
+    
+    
+    plt.show()
+
+
+def billions(cur, csvfile):
    
     data = pd.read_csv("SongPopularity.csv")
 
@@ -63,7 +121,17 @@ def main():
     cur, conn = setUpDatabase('finalprojDB.db')
     
 
-  
+    '''Calling all visualization functions.'''
+    
+    csvfile = "ArtistNextEvent.csv"
+    events_visualization(cur,csvfile)
+
+    csvfile = "TotalTopStreams.csv"
+    streams_visualzation(cur,csvfile)
+    cur.close()
+    
+    csvfile = "ArtistNextEvent.csv"
+    num_concerts_visualzation(cur,csvfile)
 
     csvfile = "SongPopularity.csv"
     billions(cur,csvfile)
